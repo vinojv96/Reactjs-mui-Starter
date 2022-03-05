b8d8582e046ce7075f24af3c2b9d5a7000ccda2c
vinojv96 <vinojv96@gmail.com>
2022-03-05 12:55:38 +0530

    App Startup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Container, Typography, Box} from '@mui/material/Container';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Reactjs mui Starter App
+        </Typography>
+      </Box>
+    </Container>
   );
 }
-
-export default App;
